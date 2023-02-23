b13de93c42ef56a509b1a72a95d6bf33e33c74fc
Maharshi2301 <mhvyas2001@gmail.com>
2023-02-23 17:18:33 +0530

    Successfully Implemented on Chase

--- OCR-PDF.py
+++ OCR-PDF.py
@@ -1,15 +1,29 @@
 import ocrmypdf
 import re
 import PyPDF2
+import csv
+import os
+import datetime
 
+now = datetime.datetime.now()
+date_time = now.strftime("%d%m%Y %H-%M-%S")
 def ocr(file_path, save_path):
     ocrmypdf.ocr(file_path,save_path, skip_text=False)
 
-text = ""
-ocr("Chase Bank.pdf","outfile6.pdf")
+input_file="D:/OCR-OUTAMATION/BOF Bank.pdf"
+input_file_name= os.path.splitext(os.path.basename(input_file))[0]
+
+output_file= 'D:/OCR-OUTAMATION/generated_output/' + input_file_name + 'outfile.pdf'
+output_folder="D:/OCR-OUTAMATION/generated_output/" + input_file_name+' '+date_time
+
+if not os.path.exists(output_folder):
+    # create folder if it deos not exist
+    os.makedirs(output_folder)
+
+# ocr(input_file,output_file)
 
 # Open the PDF file in read-binary mode
-with open('outfile6.pdf', 'rb') as f:
+with open(output_file, 'rb') as f:
     # Create a PDF reader object
     reader = PyPDF2.PdfReader(f)
 
@@ -19,25 +33,46 @@ with open('outfile6.pdf', 'rb') as f:
         page = reader.pages[page_num]
 
         # Extract the text from the page
-        text+= page.extract_text()
+        text= page.extract_text()
 
         # Print the text to the terminal
         print(text)
-
-
-
-pattern = r'((?:INSTANCES\sAMOUNT\s\n)(((\w)+\s)+[\W\d]+\s)+\n)'
-match = re.search(pattern, text)
-
-
-# Extract the data if a match was found
-# if match:
-    # data = match.group(1)
-    # ending_balance = match.group(2)
+        
+        # print INSTANCES AMOUNT
+        IST_AMT = re.findall(r'(Beginning Balance(?:.*\n)*?Ending Balance.*\n)',text,re.MULTILINE | re.IGNORECASE)
+        groups = re.findall(r'([a-zA-Z]+\s[\W]*(?:[a-zA-Z]+\s)*)(\d+)*(\s*[-$]*\d*[,.]\d+)*',''.join(IST_AMT))
+        
+        if IST_AMT:
+            print("Match Successful")
+            
+            for row in groups:
+                print(row[0])
+                
+            # print(type(groups))
+            
+                path='D:/OCR-OUTAMATION/generated_output/' + input_file_name + '/Checking_Summary++PAGE_' +str(page_num)+ '.csv'
+             
+            # Open a new CSV file in write mode
+                with open(path, 'w', newline='') as csvfile:
     
-if match:
-    matched_string = match.group(1)
-    print(matched_string)
+                # Create a csv.writer object
+                        csv_writer = csv.writer(csvfile)
+                        csv_writer.writerow(['*LABEL*','*INSTANCE*','*AMOUNT*'])
+                        # write each row of  data to the CSV File
+                        for row in groups:
+                            print(row[0]+row[1]+row[2])
+                            csv_writer.writerow([row[0],row[1],row[2]])
+                        
+        else:
+           print("No Match Found")
+
+
+
+# match = re.search(pattern, text)    
+# if match:
+    # matched_string = match.group(1)
+    # print(matched_string)
+    
 
 
 # name = re.findall(r'INSTANCES\sAMOUNT\s(?:\r?\n(?:\S|\s)*)*((?:\S|\s)+)\r?\nEnding Balance\s([\d.,]+)', text)
